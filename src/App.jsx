3427bc422029517af78381c4a3954b963ca21249
Sastrapradana06 <saatrapradana@gmail.com>
2024-10-08 22:00:13 +0700

    style: halaman login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./App.css";
 
-import image2 from "/student2.svg";
 import { useNavigate } from "react-router-dom";
 
 import { handleLoginSiswa } from "./utils/api";
@@ -61,76 +60,25 @@ function App() {
   };
 
   return (
-    <div className="w-full h-[100vh] flex items-center justify-center gap-3 bg-gray-900">
+    <div className="w-full h-[100vh] flex items-center justify-center gap-3 bg-slate-100">
       <Alert status={status} type={data.type} message={data.message} />
       {isLoading ? <Loading /> : null}
-      <div className="w-full h-max">
-        <div className="flex flex-col justify-center items-center gap-2 border border-black w-[90%] py-6 rounded-lg bg-zinc-100 m-auto lg:w-[35%]">
-          <div className="flex justify-center items-center gap-2 w-full text-indigo-400">
-            <img src={image2} alt="" width={50} />
-            <p className="font-medium text-violet-600">ClassCrafter.com</p>
+      <div className="w-[90%] h-max lg:w-[80%]  flex items-center justify-center bg-white gap-5 rounded-lg">
+        <div className="w-[50%] h-[500px]  hidden lg:flex justify-center items-center rounded-lg overflow-hidden">
+          <img src="/bg-1.jpg" alt="img" className="w-full object-cover" />
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-4  w-[90%] py-6 rounded-lg   lg:w-[50%]">
+          <div className="">
+            <h1 className="text-[1.3rem] text-black mb-1 font-semibold tracking-[2px] text-center">
+              Login Siswa
+            </h1>
+            <p className="text-gray-500 text-[.9rem] w-[400px] text-center">
+              Masuk ke akun anda, dengan mengisi email dan password
+            </p>
           </div>
 
           <FormLogin handleLogin={handleLogin} />
-          {/* <form className=" mt-3 w-[85%] lg:w-[90%]" onSubmit={handleLogin}>
-            <div className="mb-5">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-500 "
-              >
-                Masukkan email
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full border p-3 outline-[#4D44B5]  rounded-lg text-[.9rem]  bg-gray-700"
-                placeholder="name@gmail.com"
-                required
-              />
-            </div>
-            <div className="w-full flex flex-col text-[.9rem]">
-              <label
-                htmlFor="password"
-                className="block mb-2 text-sm font-medium text-gray-600 "
-              >
-                Masukkan Password <span className="text-[crimson]">*</span>
-              </label>
-              <div className="relative w-full">
-                <input
-                  type={showPassword ? "text" : "password"}
-                  name="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                  placeholder="Masukkan Password baru"
-                  className="w-full border p-3 outline-[#4D44B5] text-[.9rem] rounded-lg  bg-gray-700"
-                />
-                <div
-                  className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer"
-                  onClick={() => setShowPassword(!showPassword)}
-                >
-                  {showPassword ? (
-                    <BsEmojiHeartEyesFill
-                      className="text-violet-500"
-                      size={20}
-                    />
-                  ) : (
-                    <PiSmileyXEyesFill size={23} className="text-violet-500" />
-                  )}
-                </div>
-              </div>
-            </div>
-
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 mt-5 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-            >
-              Masuk
-            </button>
-          </form> */}
         </div>
       </div>
     </div>
